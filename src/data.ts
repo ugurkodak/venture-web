0411793007341008ee96753a03e792a7f19dd032
Ugur Kodak <ugurkodak@gmail.com>
2018-12-18 08:26:29 -0500

    Rethinking data module as a singleton. Login and register works again.

--- src/data.ts
+++ src/data.ts
@@ -2,229 +2,208 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
 
-const app = firebase.initializeApp({
+
+const database = firebase.initializeApp({
     apiKey: "AIzaSyAO8agjt4l-vPGeHR85RteTU65Eq229p00",
     authDomain: "venture-196117.firebaseapp.com",
     databaseURL: "https://venture-196117.firebaseio.com",
     projectId: "venture-196117",
     storageBucket: "venture-196117.appspot.com",
     messagingSenderId: "356906761499"
-});
-const dbRoot = app.database().ref();
+}).database().ref();
 
-export class City {
-    static db: firebase.database.Reference = dbRoot.child('city');
-    // private _id: string | null = null;
-    private _data: City.Data | null = null;
-    get data(): City.Data {
-        if (this._data != null) return this._data;
-        else {
-            console.error('City data uninitialized.');
-            return {
-                name: "",
-                tiles: []
-            };
+export class Data {
+    private static _instance: Data;
+    public user: User | null;
+    public character: Character | null;
+    // public city: City | null;
+    constructor() {
+        this.user = null;
+        this.character = null;
+        // this.city = null;
+    }
+    static getInstance(): Data {
+        if (!Data._instance) {
+            Data._instance = new Data();
+        }
+        return Data._instance;
+    }
+
+    public async register(uid: string, characterInfo: Data.Character.Info): Promise<void> {
+        try {
+            let characterId = database.child('character').push().key as string;
+            console.log(characterId);
+            await database.child('character').child(characterId).child('info').set(characterInfo);
+            console.log(uid);
+            await database.child('user').child(uid).child('info').set({
+                characterId: characterId
+            });
+            this.character = new Character(characterId, characterInfo); //TODO: Should this be here?
+        }
+        catch (error) {
+            console.error(error);
         }
     }
-    public load(data: City.Data) {
-        this._data = data;
-    }
-    static async create(areas: number, tilesPerLot = 16, lotsPerBlock = 4, blocksPerArea = 6): Promise<City.Data> {
-        let tiles: City.Tile[] = [];
-        for (let area = 0; area < areas; area++) {
-            for (let block = 0; block < blocksPerArea; block++) {
-                for (let lot = 0; lot < lotsPerBlock; lot++) {
-                    for (let tile = 0; tile < tilesPerLot; tile++) {
 
-                        //TODO: Add lot block and area info to tile positions
-                        let x = tile % Math.sqrt(tilesPerLot);
-                        let y = Math.floor(tile / Math.sqrt(tilesPerLot));
-
-                        tiles.push({
-                            x: x,
-                            y: y,
-                            rotation: 0,
-                            lotId: lot,
-                            blockId: block,
-                            areaId: area,
-                            texture: City.TileTexture.Land,
-                            characterId: City.GOVERNMENT_CHARACTER_ID,
-                            value: City.LOT_MAX_VALUE / tilesPerLot
-                        });
-                    }
+    public async login(): Promise<string | null> {
+        try {
+            let googleUser = (await firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())).user;
+            if (!googleUser) {
+                console.error('Could not login user.');
+                return null;
+            }
+            else {
+                let userInfo = (await database.child('user').child(googleUser.uid).child('info').once('value')).val() as Data.User.Info;
+                if (!userInfo) return googleUser.uid;
+                else {
+                    this.user = new User(googleUser.uid, userInfo);
+                    let characterInfo = (await database.child('character').child(userInfo.characterId).child('info').once('value')).val() as Data.Character.Info;
+                    this.character = new Character(userInfo.characterId, characterInfo); //TODO: Should this be here?
+                    return googleUser.uid;
                 }
             }
         }
-        return {
-            name: "City Name",
-            tiles: tiles
+        catch (error) {
+            console.error(error);
+            return null;
         }
     }
 }
-export namespace City {
-    export const LOT_MAX_VALUE = 500000;
-    export const GOVERNMENT_CHARACTER_ID = "gov";
+export namespace Data {
+    export namespace User {
+        export interface Info {
+            characterId: string,
+        }
+    }
+
+    export namespace Character {
+        export interface Info {
+            firstName: string;
+            lastName: string;
+            prefix: Prefix;
+        }
+        export enum Prefix {
+            MR = 'Mr.',
+            MS = 'Ms.',
+            MX = 'Mx.'
+        }
+    }
+}
+
+class User {
+    private readonly _collection: firebase.database.Reference;
+    public id: string;
+    public _info: Data.User.Info;
+    private _data: User.Data | null;
+
+    constructor(id: string, info: Data.User.Info) {
+        this._collection = database.child('user');
+        this.id = id;
+        this._info = info;
+        this._data = null;
+    }
+}
+
+namespace User {
     export interface Data {
-        name: string;
-        tiles: Tile[];
-    }
-    export enum TileTexture {
-        Road,
-        Tramway,
-        Land
-    }
-    export interface Tile {
-        x: number;
-        y: number;
-        rotation: number;
-        lotId: number;
-        blockId: number;
-        areaId: number;
-        texture: TileTexture;
-        characterId: string;
-        value: number;
+
     }
 }
-export class Character {
-    static db: firebase.database.Reference = dbRoot.child('character');
-    private _id: string | null = null;
-    private _prefix: Character.Prefix | null = null;
-    private _firstName: string | null = null;
-    private _lastName: string | null = null;
-    private _balance: number | null = null;
 
-    get id(): string {
-        if (this._id != null) return this._id;
-        else {
-            console.error('Uninitialized character id.');
-            return '';
-        }
-    }
-    get prefix(): Character.Prefix {
-        if (this._prefix != null) return this._prefix;
-        else {
-            console.error('Uninitialized character prefix.');
-            return Character.Prefix.MX;
-        }
-    }
-    set prefix(prefix: Character.Prefix) {
-        this._prefix = prefix;
-    }
-    get firstName(): string {
-        if (this._firstName != null) return this._firstName;
-        else {
-            console.error('Uninitialized character firstName.');
-            return '';
-        }
-    }
-    set firstName(firstName: string) {
-        this._firstName = firstName;
-    }
-    get lastName(): string {
-        if (this._lastName != null) return this._lastName;
-        else {
-            console.error('Uninitialized character lastName.');
-            return '';
-        }
-    }
-    set lastName(lastName: string) {
-        this._lastName = lastName;
-    }
-    get balance(): number {
-        if (this._balance != null) return this._balance;
-        else {
-            console.error('Uninitialized character balance');
-            return 0;
-        }
-    }
-    set balance(balance: number) {
-        this._balance = balance;
-    }
+class Character {
+    private readonly _collection: firebase.database.Reference;
+    public id: string;
+    public _info: Data.Character.Info;
+    private _data: Character.Data | null;
 
-    public async load(characterId: string) {
-        try {
-            let snap = await Character.db.child(characterId).once('value');
-            let data = snap.val() as Character.Data;
-            this._id = characterId;
-            this.populateData(data);
-        }
-        catch (error) {
-            console.error('Failed to load character.');
-        }
-    }
-
-    /** Creates a character entry in the database for the first time. */
-    public async create(data: Character.Data) {
-        try {
-            this._id = (await Character.db.push(data)).key;
-            this.populateData(data);
-        }
-        catch (error) {
-            console.error('Failed to create character.');
-        }
+    constructor(id: string, info: Data.Character.Info) {
+        this._collection = database.child('character');
+        this.id = id;
+        this._info = info;
+        this._data = null;
     }
 
     public getFullName(): string {
-        return this.prefix + ' ' + this.firstName + ' ' + this.lastName;
-    }
-
-    private populateData(data: Character.Data) {
-        this.prefix = data.prefix;
-        this.firstName = data.firstName;
-        this.lastName = data.lastName;
-        this.balance = data.balance;
+        return this._info.prefix + ' ' + this._info.firstName + ' ' + this._info.lastName;
     }
 }
-export namespace Character {
+
+namespace Character {
     export interface Data {
-        prefix: Prefix;
-        firstName: string;
-        lastName: string;
         balance: number;
     }
-
-    export const INITIAL_BALANCE = 1000000;
-
-    export enum Prefix {
-        MR = 'Mr.',
-        MS = 'Ms.',
-        MX = 'Mx.'
-    }
 }
-export class User {
-    static db: firebase.database.Reference = dbRoot.child('user');
-    public characterId: any | null = null;
-    private _googleUser: firebase.User | null = null;
 
-    /** Login and set character id if exists */
-    public async login(): Promise<null | Error> {
-        try {
-            this._googleUser = (await firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())).user;
-            if (this._googleUser == null) return new Error('Could not get google user.');
-            else {
-                let snap = await User.db.child(this._googleUser.uid).child('characterId').once('value');
-                if (snap.exists()) this.characterId = snap.val();
-                return null;
-            }
-        }
-        catch (error) {
-            return error;
-        }
-    }
+// class City {
+//     static db: firebase.database.Reference = dbRoot.child('city');
+//     // private _id: string | null = null;
+//     private _data: City.Data | null = null;
+//     get data(): City.Data {
+//         if (this._data != null) return this._data;
+//         else {
+//             console.error('City data uninitialized.');
+//             return {
+//                 name: "",
+//                 tiles: []
+//             };
+//         }
+//     }
+//     public load(data: City.Data) {
+//         this._data = data;
+//     }
+//     static async create(areas: number, tilesPerLot = 16, lotsPerBlock = 4, blocksPerArea = 6): Promise<City.Data> {
+//         let tiles: City.Tile[] = [];
+//         for (let area = 0; area < areas; area++) {
+//             for (let block = 0; block < blocksPerArea; block++) {
+//                 for (let lot = 0; lot < lotsPerBlock; lot++) {
+//                     for (let tile = 0; tile < tilesPerLot; tile++) {
 
-    /** Registers a new user with characterId foreign key */
-    public async register(characterId: string): Promise<null | Error> {
-        try {
-            this.characterId = characterId;
-            if (this._googleUser != null) {
-                await User.db.child(this._googleUser.uid).set({
-                    characterId: this.characterId
-                });
-            }
-            return null;
-        }
-        catch (error) {
-            return error;
-        }
-    }
-}
+//                         //TODO: Add lot block and area info to tile positions
+//                         let x = tile % Math.sqrt(tilesPerLot);
+//                         let y = Math.floor(tile / Math.sqrt(tilesPerLot));
+
+//                         tiles.push({
+//                             x: x,
+//                             y: y,
+//                             rotation: 0,
+//                             lotId: lot,
+//                             blockId: block,
+//                             areaId: area,
+//                             texture: City.TileTexture.Land,
+//                             characterId: City.GOVERNMENT_CHARACTER_ID,
+//                             value: City.LOT_MAX_VALUE / tilesPerLot
+//                         });
+//                     }
+//                 }
+//             }
+//         }
+//         return {
+//             name: "City Name",
+//             tiles: tiles
+//         }
+//     }
+// }
+// namespace City {
+//     export const LOT_MAX_VALUE = 500000;
+//     export const GOVERNMENT_CHARACTER_ID = "gov";
+//     export interface Data {
+//         name: string;
+//         tiles: Tile[];
+//     }
+//     export enum TileTexture {
+//         Road,
+//         Tramway,
+//         Land
+//     }
+//     export interface Tile {
+//         x: number;
+//         y: number;
+//         rotation: number;
+//         lotId: number;
+//         blockId: number;
+//         areaId: number;
+//         texture: TileTexture;
+//         characterId: string;
+//         value: number;
+//     }
+// }
